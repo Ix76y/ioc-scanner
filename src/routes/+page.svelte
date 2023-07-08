8cff72e828761bc06e06531d06045a4f19405a82
Iz76y <is.hoekstein@gmail.com>
2023-07-08 23:46:08 +0100

    project setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    document.body.classList.add('bg-zinc-100');
+    document.body.classList.add('dark:bg-zinc-900');
+</script>
+
+<div class="p-8 bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100">
+    <div class="bg-zinc-200 dark:bg-zinc-800 rounded overflow-hidden shadow-lg">
+        <p class="text-gray-700 dark:text-gray-300 text-base p-16">
+        Work In Progress...  💻
+        </p>
+        
+    </div>
+</div>
