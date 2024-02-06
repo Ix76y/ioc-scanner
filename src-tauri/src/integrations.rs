1c5443bf603ab525e3b8e2daa181f0dbec0cb9ff
Iz76y <is.hoekstein@gmail.com>
2024-02-06 23:40:57 +0000

    manage keys over UI and show configured integrations

--- src-tauri/src/integrations.rs
+++ src-tauri/src/integrations.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
+use crate::secrets::{get_keys, get_secret};
+
 #[derive(Serialize, Deserialize)]
 pub enum Status {
     Planned,
@@ -14,54 +16,40 @@ pub struct Integration {
     pub name: String,
     image: String, 
     url: String,
-    status: Status
+    status: Status,
+    pub configured: bool,
+    pub apikey: String
+}
+
+fn build_integration(name: String, image: String, url: String, status: Option<Status>) -> Integration {
+    Integration {
+        name,
+        image,
+        url,
+        status: status.unwrap_or(Status::Planned),
+        configured: false,
+        apikey: "".to_owned()
+    }
 }
 
 #[tauri::command]
 pub fn get_integrations() -> [Integration; 7] {
-    let integrations = [
-        Integration {
-            name: "URLScan.io".to_owned(),
-            image: "urlscan-logo.png".to_owned(),
-            url: "https://urlscan.io/".to_owned(),
-            status: Status::InProgress
-        },
-        Integration {
-            name: "EmailRep".to_owned(),
-            image: "emailrep-logo.png".to_owned(),
-            url: "https://emailrep.io/".to_owned(),
-            status: Status::InProgress
-        },
-        Integration {
-            name: "IPInfo.io".to_owned(),
-            image: "ipinfo-logo.png".to_owned(),
-            url: "https://ipinfo.io/".to_owned(),
-            status: Status::InProgress
-        },
-        Integration {
-            name: "VirusTotal".to_owned(),
-            image: "vt-logo.png".to_owned(),
-            url: "https://www.virustotal.com/gui/home/upload".to_owned(),
-            status: Status::Planned
-        },
-        Integration {
-            name: "IPLocation".to_owned(),
-            image: "iplocation-logo.png".to_owned(),
-            url: "https://www.iplocation.net/".to_owned(),
-            status: Status::Planned
-        },
-        Integration {
-            name: "AbuseIPDB".to_owned(),
-            image: "abuseipdb-logo.png".to_owned(),
-            url: "https://www.abuseipdb.com/".to_owned(),
-            status: Status::Planned
-        },
-        Integration {
-            name: "HaveIBeenPwned".to_owned(),
-            image: "haveibeenpwned-logo.png".to_owned(),
-            url: "https://haveibeenpwned.com/".to_owned(),
-            status: Status::Planned
-        },
+    let mut integrations = [
+        build_integration("URLScan.io".to_owned(), "/src/lib/assets/urlscan-logo.png".to_owned(), "https://urlscan.io/".to_owned(), Some(Status::InProgress)),
+        build_integration("EmailRep".to_owned(), "/src/lib/assets/emailrep-logo.png".to_owned(), "https://emailrep.io/".to_owned(), Some(Status::InProgress)),
+        build_integration("IPInfo.io".to_owned(), "/src/lib/assets/ipinfo-logo.png".to_owned(), "https://ipinfo.io/".to_owned(), Some(Status::InProgress)),
+        build_integration("VirusTotal".to_owned(), "/src/lib/assets/vt-logo.png".to_owned(), "https://www.virustotal.com/gui/home/upload".to_owned(), None),
+        build_integration("IPLocation".to_owned(), "/src/lib/assets/iplocation-logo.png".to_owned(), "https://www.iplocation.net/".to_owned(), None),
+        build_integration("AbuseIPDB".to_owned(), "/src/lib/assets/abuseipdb-logo.png".to_owned(), "https://www.abuseipdb.com/".to_owned(), None),
+        build_integration("HaveIBeenPwned".to_owned(), "/src/lib/assets/haveibeenpwned-logo.png".to_owned(), "https://haveibeenpwned.com/".to_owned(), None)
     ];
+    let configured_integrations = get_keys();
+    for integration in integrations.iter_mut() {
+        if configured_integrations.contains(&integration.name) {
+            integration.configured = true;
+            let secret = get_secret(&integration.name);
+            integration.apikey = secret;
+        }
+    }
     integrations
 }
--- src/lib/pages/KeysPage.svelte
+++ src/lib/pages/KeysPage.svelte
@@ -1,20 +1,29 @@
 <script>
     import { invoke } from '@tauri-apps/api/tauri';
+    import { fade } from 'svelte/transition';
 
     const rowHeight = 32;
 
     let hasStore = false;
     let storeError = "";
 
+    // variable to store which intgration is currently open if any
+    let openedIndex = -1;
+
     /**
-     * @type {string[]}
+     * @type {any}
      */
-    let configuredIntegrations = [];
+    let integrations = [];
 
     async function getIntegrations() {
         createSecretStore();
-        configuredIntegrations = await invoke('get_keys');
-        console.log('Configured Integrations: ' + configuredIntegrations);
+        // configuredIntegrations = await invoke('get_keys');
+        // console.log('Configured Integrations: ' + configuredIntegrations);
+        integrations = await invoke('get_integrations');
+        /*console.log('All Integrations: ');
+        for (const i of integrations) {
+            console.log(`${i.name}: ${i.configured}`);
+        }*/
     }
 
     async function createSecretStore() {
@@ -28,9 +37,24 @@
         }
     }
 
+    /**
+     * @param {number} i
+     */
+    async function testConnection(i) {
+        console.log(`Testing connection for ${integrations[i].name}`);
+    }
+
+    /**
+     * @param {number} i
+     */
+    async function saveIntegration(i) {
+        let saved = await invoke('update_secret', {key: integrations[i].name, secret: integrations[i].apikey});
+        console.log("Key successfully saved! Getting integrations again to update UI?");
+        getIntegrations();
+    }
+
+
     getIntegrations();
-
-
 </script>
 
 <div class="p-4 bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100">
@@ -42,19 +66,48 @@
     </div>
     {#if hasStore }
         <div>
-            {#each configuredIntegrations as integration}
-            <div class="bg-white dark:bg-zinc-800 hover:bg-zinc-50 hover:dark:bg-zinc-700 rounded overflow-hidden flex flew-row p-3 my-2 items-center">
-                <img src={integration.img} class="mx-auto" alt="logo" width="{rowHeight}px" height="{rowHeight}px" />
-
-                <p class="grow mx-2 inline-block align-middle "  style="height='{rowHeight}px'">{integration.name} <!--<a href="{integration.url}" target="_blank"><ion-icon name="link-outline"></ion-icon></a>--></p>
-                {#if integration.status == 'Not Started'}
-                    <p class="inline-block align-middle font-extralight text-xs text-slate-600 dark:text-slate-400">{integration.status}</p>
-                {:else if integration.status == 'In Progress'}
-                    <p class="inline-block align-middle font-extralight text-xs text-amber-600 dark:text-amber-400">{integration.status}</p>
-                {:else if integration.status == 'Done'}
-                    <p class="inline-block align-middle font-extralight text-xs text-green-600 dark:text-green-400">{integration.status}</p>
-                {:else}
-                    <p class="inline-block align-middle font-extralight text-xs text-purple-600 dark:text-purple-400">{integration.status}</p>
+            {#each integrations as integration, i}
+            <div class="divide-y divide-dashed divide-zinc-100 dark:divide-zinc-600 bg-white dark:bg-zinc-800 rounded overflow-hidden my-2 ">
+                <div class=" flex flew-row items-center p-3">
+                    <img src={integration.image} class="mx-auto" alt="logo" width="{rowHeight}px" height="{rowHeight}px" />
+                    <!--     Planned, InProgress, Implemented, Requested-->
+                    <p class="grow mx-2 inline-block align-middle "  style="height='{rowHeight}px'">{integration.name} <!--<a href="{integration.url}" target="_blank"><ion-icon name="link-outline"></ion-icon></a>--></p>
+                    
+                    <!-- TODO: show button when closed, otherwise show x; if x is clicked reset input? -->
+                    <button class="text-sky-500 hover:text-gray-700 hover:dark:text-white font-extralight text-sm hover:font-light hover:underline underline-offset-2 decoration-8 decoration-sky-500" on:click={() => openedIndex = i}>
+                        {#if integration.configured}
+                        Edit
+                        {:else}
+                        Configure
+                        {/if}
+                    </button>
+                </div>
+                {#if i == openedIndex}
+                    <div in:fade={{ delay: 0, duration: 250 }} class="p-3">
+                        <div class="flex flex-row py-2 items-center min-w-96">
+                            <label for="api-key"
+                                class="pr-4 pointer-events-none h-full select-nonetext-sm leading-tight text-gray-600 dark:text-gray-200 transition-all  after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                                API Key: 
+                            </label>
+                            <input type="password" bind:value={integration.apikey} id="api-key" style="min-width: 320px;" class="grow mr-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500" required> 
+                            <div class="inline-flex rounded-md shadow-sm" role="group">
+                                <!-- TOOD: save/ update function on click & button style-->
+                                <button type="button" on:click={() => saveIntegration(i)} class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                                    Save
+                                </button>
+                                <!--<button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                                    Settings
+                                </button>-->
+                                <!-- TOOD: disbaled button style & test function & show checkmark after test -->
+                                <button type="button" disabled={!integration.configured && integration.apikey.length == 0} on:click={() => testConnection(i)} class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white disabled:cursor-not-allowed">
+                                    Test
+                                </button>
+                            </div>
+                        </div>
+                        {#if integration.configured}
+                            <p>Last Successfull Connection: 2023/02/06</p>
+                        {/if}
+                    </div>
                 {/if}
             </div>
         {/each}
