790f20c8e9576945404e13f7a67fb4e411f052a9
Iz76y <is.hoekstein@gmail.com>
2024-05-19 17:15:32 +0100

    Update settings to choose key store location

--- src/lib/pages/SettingsPage.svelte
+++ src/lib/pages/SettingsPage.svelte
@@ -1,11 +1,14 @@
 <script>
     import { invoke } from '@tauri-apps/api/tauri';
+    import { save } from '@tauri-apps/api/dialog';
     import { Store } from "tauri-plugin-store-api";
 
     const store = new Store("ioc-scanner.bin");
     const secureScanKey = "SecureScan";
+    const secureStorePathKey = "SecureStorePath";
 
     let secureScan = false;
+    let secureStorePath = "Not Set";
 
     // update store if value changes
     const secureScanChange = async () => {
@@ -13,6 +16,20 @@
         store.set(secureScanKey, {value: secureScan});
         await store.save();
         console.log(`Saved store.`);
+    }
+
+    // show save dialog
+    const saveStore = async () => {
+        const filePath = await save({
+            filters: []
+        });
+
+        if (filePath) {
+            secureStorePath = filePath;
+            store.set(secureStorePathKey, {value: filePath});
+            await store.save();
+            console.log(`Saved user selected file path: ${filePath}`);
+        }
     }
 
 
@@ -27,20 +44,38 @@
         } else {
             secureScan = tmp.value;
         }
+
+        tmp = await store.get(secureStorePathKey);
+        if (tmp) {
+            secureStorePath = tmp.value;
+        }
     }
     load_data();
 </script>
 
 <div class="p-4 bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100">
-    <div class="bg-zinc-200 dark:bg-zinc-800 rounded overflow-hidden shadow-lg p-4">
+    <div class="bg-white dark:bg-zinc-800 rounded overflow-hidden my-2 p-4">
         <h2>General</h2>
         <div class="flex items-center mb-4">
-            <input id="default-checkbox" type="checkbox" bind:checked={secureScan} on:change={secureScanChange} value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-            <div class="ms-2 mt-5">
+            <input id="default-checkbox" type="checkbox" bind:checked={secureScan} on:change={secureScanChange} value="" class="mb-3 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+            <div class="ms-2 mt-2">
                 <label for="default-checkbox" class="text-sm font-medium text-gray-900 dark:text-gray-300">Secure Scan</label>
                 <p class="text-sm text-gray-700 dark:text-gray-500">When checked IOCs are not submitted to any tools, and this app will only do lookups.</p>
             </div>
         </div>
-        <h2>Summary</h2>
+        <h2 class="mt-2 mb-1">Key Store</h2>
+        <div class="flex flew-row">
+            <div class="grow">
+                <div class="">{secureStorePath}</div>
+                <p class="text-sm text-gray-700 dark:text-gray-500 select-none cursor-default">File to store all your API Keys and credentials securely.</p>
+            </div>
+            <button
+                on:click={saveStore}
+                class="align-middle select-none font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-xs border border-gray-900 dark:border-gray-100 text-gray-900 dark:text-gray-100 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] flex items-center gap-1"
+                type="button">
+                <ion-icon class="text-lg" name="refresh-outline"></ion-icon>
+                Update
+            </button>
+        </div>
     </div>
 </div>
